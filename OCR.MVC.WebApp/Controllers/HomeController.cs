32f004c5f00124a8f853f930284002d3ccd87c5b
LUBISAL1 <LUBISAL1@ALIDBA-L11052>
2021-12-21 18:13:06 +0700

    Add project files.

---
+++ OCR.MVC.WebApp/Controllers/HomeController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.IO;
+using Tesseract;
+
+namespace OCR.MVC.WebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public const string folderName = "images/";
+        public const string fileName = "download.jpg";
+        public const string trainedDataFolderName = "tessdata";
+        public IActionResult Index()
+        {
+            ViewBag.ShowResult = false;
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult SaveCapture(string data)
+        {
+            byte[] imageBytes = Convert.FromBase64String(data.Split(',')[1]);
+            MemoryStream stream = new(imageBytes);
+            IFormFile file = new FormFile(stream, 0, imageBytes.Length, "download", "download" + ".jpg");
+            var filePath = Path.Combine(folderName, fileName);
+            using (FileStream fs = System.IO.File.Create(filePath))
+            {
+                file.CopyTo(fs);
+            }
+
+            var result = ScanImage();
+            return Json(new { result });
+        }
+
+        public String ScanImage()
+        {
+            string tessPath = Path.Combine(trainedDataFolderName, "");
+            var engine = new TesseractEngine(tessPath, "eng", EngineMode.Default);
+            var img = Pix.LoadFromFile(folderName + fileName);
+
+            var page = engine.Process(img);
+            string result = page.GetText();
+            return result;
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+    }
+}
---
+++ OCR.MVC.WebApp/Controllers/MastersController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace OCR.MVC.WebApp.Controllers
+{
+    public class MastersController : Controller
+    {
+        public IActionResult Medicines()
+        {
+            return View();
+        }
+        public IActionResult Doctors()
+        {
+            return View();
+        }
+        public IActionResult Patients()
+        {
+            return View();
+        }
+    }
+}
